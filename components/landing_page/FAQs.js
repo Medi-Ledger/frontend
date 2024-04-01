d6e9e1fb9bdac4ebafec1f7fb9477095fffe5cf0
Ujjwal Singhm <singhujjwal231@gmail.com>
2024-04-02 01:16:49 +0530

    Added faq section.

--- components/landing_page/FAQs.js
+++ components/landing_page/FAQs.js
@@ -1,23 +1,86 @@
-import React from 'react'
+
+"use client"
+import React, { useState } from 'react';
+import Image from 'next/image'; 
 
 const FAQs = () => {
+  const [isAnswerVisible, setIsAnswerVisible] = useState({
+    isSecure: false,
+    isAccess: false,
+    isCompliant: false,
+    isUpdate: false
+  });
+
+  const toggleAnswerVisibility = (question) => {
+    setIsAnswerVisible({
+      ...isAnswerVisible,
+      [question]: !isAnswerVisible[question]
+    });
+  };
+
   return (
-    <section className="bg-gray-200 py-16">
-      <div className="container mx-auto">
-        <h2 className="text-3xl font-bold text-center mb-4">Frequently Asked Questions</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <div className="text-lg">
-            <h3 className="font-bold mb-2">Is my data secure on MediLedger?</h3>
-            <p>Yes, MediLedger uses blockchain technology to ensure the security and integrity of patient records.</p>
+    <section className="bg-gray-200 py-10">
+      <div className="text-center"> 
+            <h2 className="text-3xl font-bold mb-10">Frequently Asked Questions (FAQ's)</h2>
           </div>
-          <div className="text-lg">
-            <h3 className="font-bold mb-2">How can I access my patient records?</h3>
-            <p>Patients can easily access their records by logging into their MediLedger account.</p>
+      <div className="container px-10 mx-auto flex">
+        <div className="md:w-1/2 flex flex-col gap-8 ml-4"> 
+          
+          <div className="bg-[#F0F0F2] rounded-lg shadow-sm p-4 mb-4">
+            <h3 className="font-bold mb-2 cursor-pointer" onClick={() => toggleAnswerVisibility('isSecure')}>
+              &#8226; Is my data secure on MediLedger?
+            </h3>
+            {isAnswerVisible.isSecure && (
+              <p>Yes, MediLedger uses blockchain technology to ensure the security and integrity of patient records.</p>
+            )}
           </div>
+          <div className="bg-[#F0F0F2] rounded-lg shadow-sm p-4 mb-4">
+            <h3 className="font-bold mb-2 cursor-pointer" onClick={() => toggleAnswerVisibility('isAccess')}>
+              &#8226; How can I access my patient records?
+            </h3>
+            {isAnswerVisible.isAccess && (
+              <p>Patients can easily access their records by logging into their MediLedger account.</p>
+            )}
+          </div>
+          <div className="bg-[#F0F0F2] rounded-lg shadow-sm p-4 mb-4">
+            <h3 className="font-bold mb-2 cursor-pointer" onClick={() => toggleAnswerVisibility('isCompliant')}>
+              &#8226; Is the system compliant with healthcare regulations and standards?
+            </h3>
+            {isAnswerVisible.isCompliant && (
+              <p>Yes, our system is designed to comply with relevant healthcare regulations and standards, such as HIPAA (Health Insurance Portability and Accountability Act) in the United States.</p>
+            )}
+          </div>
+          <div className="bg-[#F0F0F2] rounded-lg shadow-sm p-4 mb-4">
+            <h3 className="font-bold mb-2 cursor-pointer" onClick={() => toggleAnswerVisibility('isUpdate')}>
+              &#8226; Can patients update their own records through the system?
+            </h3>
+            {isAnswerVisible.isUpdate && (
+              <p>Yes, patients have the ability to update certain aspects of their records, such as personal information and medical history, through their secure patient portal.</p>
+            )}
+          </div>
+        </div>
+        <div className="md:w-1/2 flex items-start justify-center"> 
+          <Image src="/think.png" height={450} width={450} />
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default FAQs
+export default FAQs;
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
--- components/landing_page/HeroImg.js
+++ components/landing_page/HeroImg.js
@@ -6,7 +6,7 @@ const HeroImg = () => {
     <div className="flex justify-center items-center relative rounded-md mt-5">
       <div className="absolute bottom-0 left-0 bg-[#FFC671] w-3/4 rounded-tr-md z-10" style={{height: "470px"}}></div>
       <div className='absolute top-0 left-20 border-y border-l border-gray-400 h-full w-full z-0 grid place-items-center'>
-        <div class="grid grid-cols-3 grid-rows-3 w-full h-full">
+        <div className="grid grid-cols-3 grid-rows-3 w-full h-full">
             <div className="border-r border-b border-gray-400 hover:border-0"></div>
             <div className="border-b border-gray-400 hover:border-0"></div>
             <div className="border-x border-b border-gray-400 hover:border-0"></div>
